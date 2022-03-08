7971ef2f40978fcabcb1be794b9e5a9b30c8a9f1
Weslley Prado <weslley.prado73@gmail.com>
2022-03-08 13:24:19 -0300

    This menu was created

--- frontend/src/components/templates/Nav.jsx
+++ frontend/src/components/templates/Nav.jsx
@@ -3,5 +3,14 @@ import React from 'react'
 
 export default props => 
         <aside className="menu-area">
-           Navegacao     
+           <nav className="menu">
+              {/*Refatorar cdigo*/}
+             <a href="#/">
+              <i className="fa fa-home"></i> Inicio
+              </a>
+              <a href="#/">
+              <i className="fa fa-users"></i> Usuario
+              </a>
+
+            </nav>     
         </aside>
