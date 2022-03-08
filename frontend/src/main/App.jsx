cb0e544a84d551767ddcd3b6f9414bab739dd208
Weslley Prado <weslley.prado73@gmail.com>
2022-03-08 15:48:23 -0300

    Nav was created

--- frontend/src/main/App.jsx
+++ frontend/src/main/App.jsx
@@ -2,17 +2,21 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'font-awesome/css/font-awesome.min.css'
 import './App.css'
 import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
 
 import Logo from '../components/templates/Logo'
 import Nav from '../components/templates/Nav'
-import Home from '../components/home/Home'
+import Routes from './Routes'
 import Footer from '../components/templates/Footer'
 
 
 export default props =>
+<BrowserRouter>
 <div className="app">
     <Logo />
     <Nav />
-    <Home />
+    <Routes />
     <Footer />
     </div>
+
+</BrowserRouter>
