90a43a32eb37508c89d9e186af52e46a70e9f75f
Weslley Prado <weslley.prado73@gmail.com>
2022-03-08 12:18:21 -0300

    This footer was created

--- frontend/src/components/templates/Footer.jsx
+++ frontend/src/components/templates/Footer.jsx
@@ -5,5 +5,5 @@ export default props =>
 
     <footer className="footer">
         Desenvolvido com <i className="fa fa-heart text danger"></i> por 
-        <strong> WHT<span className="text-danger">West High Technology</span></strong>
+        <strong> WHT <span className="text-danger"> West High Technology</span></strong>
     </footer>
